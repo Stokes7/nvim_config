aaef8d6a0cbc44deaa5b10f39f9f073f471e20f2
Stokes7 <cristianesteban8@gmail.com>
2026-06-12 16:11:21 +0200

    refactorizing LSP config

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -43,39 +43,48 @@ return {
 					-- Rename the variable under your cursor.
 					--  Most Language Servers support renaming across files, etc.
 					map("grn", vim.lsp.buf.rename, "[R]e[n]ame")
+					map("<leader>cr", vim.lsp.buf.rename, "[C]ode [R]ename")
 
 					-- Execute a code action, usually your cursor needs to be on top of an error
 					-- or a suggestion from your LSP for this to activate.
 					map("gra", vim.lsp.buf.code_action, "[G]oto Code [A]ction", { "n", "x" })
+					map("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction", { "n", "x" })
 
 					-- Find references for the word under your cursor.
 					map("grr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
+					map("<leader>cf", require("telescope.builtin").lsp_references, "[C]ode [F]ind References")
 
 					-- Jump to the implementation of the word under your cursor.
 					--  Useful when your language has ways of declaring types without an actual implementation.
 					map("gri", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
+					map("<leader>ci", require("telescope.builtin").lsp_implementations, "[C]ode [I]mplementation")
 
 					-- Jump to the definition of the word under your cursor.
 					--  This is where a variable was first declared, or where a function is defined, etc.
 					--  To jump back, press <C-t>.
 					map("grd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
+					map("<leader>cd", require("telescope.builtin").lsp_definitions, "[C]ode [D]efinition")
 
 					-- WARN: This is not Goto Definition, this is Goto Declaration.
 					--  For example, in C this would take you to the header.
 					map("grD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
+					map("<leader>cD", vim.lsp.buf.declaration, "[C]ode [D]eclaration")
 
 					-- Fuzzy find all the symbols in your current document.
 					--  Symbols are things like variables, functions, types, etc.
 					map("gO", require("telescope.builtin").lsp_document_symbols, "Open Document Symbols")
+					map("<leader>cs", require("telescope.builtin").lsp_document_symbols, "[C]ode [S]ymbols")
 
 					-- Fuzzy find all the symbols in your current workspace.
 					--  Similar to document symbols, except searches over your entire project.
 					map("gW", require("telescope.builtin").lsp_dynamic_workspace_symbols, "Open Workspace Symbols")
+					map("<leader>cw", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[C]ode [W]orkspace Symbols")
 
 					-- Jump to the type of the word under your cursor.
 					--  Useful when you're not sure what type a variable is and you want to see
 					--  the definition of its *type*, not where it was *defined*.
 					map("grt", require("telescope.builtin").lsp_type_definitions, "[G]oto [T]ype Definition")
+					map("<leader>ct", require("telescope.builtin").lsp_type_definitions, "[C]ode [T]ype Definition")
 
 					-- This function resolves a difference between neovim nightly (version 0.11) and stable (version 0.10)
 					---@param client vim.lsp.Client
@@ -86,7 +95,7 @@ return {
 						if vim.fn.has("nvim-0.11") == 1 then
 							return client:supports_method(method, bufnr)
 						else
-							return client.supports_method(method, { bufnr = bufnr })
+							return client:supports_method(method, { bufnr = bufnr })
 						end
 					end
 
@@ -133,7 +142,10 @@ return {
 					-- This may be unwanted, since they displace some of your code
 					if client and client_supports_method(client, vim.lsp.protocol.Methods.textDocument_inlayHint, event.buf) then
 						map("<leader>uh", function()
-							vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = event.buf }))
+							vim.lsp.inlay_hint.enable(
+								not vim.lsp.inlay_hint.is_enabled({ bufnr = event.buf }),
+								{ bufnr = event.buf }
+							)
 						end, "Toggle Inlay Hints")
 					end
 				end,
@@ -204,6 +216,7 @@ return {
 
 			local cuda_home = vim.env.CUDA_HOME
 				or "/cvmfs/software.hpc.rwth.de/Linux/RH9/x86_64/intel/sapphirerapids/software/CUDA/12.8.0"
+			local has_cuda = cuda_home and vim.fn.isdirectory(cuda_home) == 1
 
 			local servers = {
 				-- gopls = {},
@@ -220,23 +233,28 @@ return {
 
 				clangd = {
 					-- Good defaults; tweak as you like
-					cmd = {
-						"clangd",
-						"--background-index", -- keep an index in the background
-						"--clang-tidy", -- enable clang-tidy diagnostics
-						"--completion-style=detailed",
-						"--header-insertion=iwyu",
-						"--pch-storage=memory",
-						cuda_home and ("--cuda-path=" .. cuda_home) or nil,
-						-- If you see encoding warnings, uncomment next line:
-						-- "--offset-encoding=utf-16",
-					},
+					cmd = (function()
+						local c = {
+							"clangd",
+							"--background-index", -- keep an index in the background
+							"--clang-tidy", -- enable clang-tidy diagnostics
+							"--completion-style=detailed",
+							"--header-insertion=iwyu",
+							"--pch-storage=memory",
+							-- If you see encoding warnings, uncomment next line:
+							-- "--offset-encoding=utf-16",
+						}
+						if has_cuda then
+							table.insert(c, "--cuda-path=" .. cuda_home)
+						end
+						return c
+					end)(),
 
 					filetypes = { "c", "cpp", "objc", "objcpp", "cuda" },
 
 					settings = {
 						clangd = {
-							fallbackFlags = cuda_home and {
+							fallbackFlags = has_cuda and {
 								"-std=c++20",
 								"-I" .. cuda_home .. "/include",
 							} or {
